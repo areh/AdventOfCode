56c4de45985ff7ed60a729041ea5695c3dfe7adb
Are Hoel <1456451+areh@users.noreply.github.com>
2024-12-03 12:07:47 +0100

    Solved with matching all three parts of data that is interesting, in one go, and iterating over it

--- 2024/03/03_2.py
+++ 2024/03/03_2.py
@@ -4,30 +4,19 @@ inputfile = open("03_input.txt", "r")
 inputdata = inputfile.read()
 inputfile.close()
 
+
+compilation = re.compile(r"mul\((\d{1,3})\,(\d{1,3})\)|do\(\)|don't\(\)")
+
 total = 0
+include = True
 
-outerex = re.split(r"don't\(\)", inputdata)
-
-first = True
-
-for outer in outerex:
-  if first:
-    multext = outer
-    first = False
-  else:
-    inner = re.search(r"do\(\)(.*)", outer)
-    if inner:
-      multext = inner.group()
-    else:
-      multext = None
-
-  if multext:
-    matches = re.findall(r"mul\((\d{1,3})\,(\d{1,3})\)", multext)
-    for match in matches:
-      calc = int(match[0])*int(match[1])
-#      print(str(match[0]) + " * " + str(match[1]) + " = " + str(calc))
-      total += calc
-    matches.clear()
+for outer in compilation.finditer(inputdata):
+  if outer.group() == "do()":
+    include = True
+  elif outer.group() == "don't()":
+    include = False
+  elif include:
+    total += int(outer.group(1)) * int(outer.group(2))
 
 print("The total is: " + str(total))
 
