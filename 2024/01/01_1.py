59c6785ac0b1a0fbe643717fd1dcf05182326d2a
Are Hoel <1456451+areh@users.noreply.github.com>
2024-12-03 11:16:33 +0100

    Moved into year folder

---
+++ 2024/01/01_1.py
@@ -0,0 +1,27 @@
+inputFile = open("01_input.txt", "r")
+lines = inputFile.readlines()
+
+listA = []
+listB = []
+
+for line in lines:
+    linesplit = line.split()
+    listA.append(linesplit[0])
+    listB.append(linesplit[1])
+
+listA.sort()
+listB.sort()
+
+distance = 0
+pos = 0
+for a in listA:
+    distance += abs(int(a) - int(listB[pos]))
+#    if (a > listB[pos]):
+#        notice = "A > B"
+#    else:
+#        notice = "OK"
+#    print("A: " + a + " :: B: " + listB[pos] + " :: " + notice)
+    pos += 1
+
+print("Distance: " + str(distance) + " ?")
+
